9d9ef7bfde600bb504126a25fc20b36574014ee5
MCB-SMART-BOY <mcb2720838051@gmail.com>
2026-01-31 21:18:28 +0800

    chore: bump neve to 1.0.0

--- Formula/neve.rb
+++ Formula/neve.rb
@@ -1,18 +1,18 @@
 class Neve < Formula
   desc "A pure functional language for system configuration and package management"
   homepage "https://github.com/MCB-SMART-BOY/Neve"
-  version "0.7.0"
+  version "1.0.0"
   license "MPL-2.0"
 
   on_macos do
     on_intel do
-      url "https://github.com/MCB-SMART-BOY/Neve/releases/download/v0.7.0/neve-x86_64-apple-darwin.tar.gz"
-      sha256 "be568a0938df0331a0bc6ab41a0fa5c11290d1b9c3fa24e1df8fc3f0060daee9"
+      url "https://github.com/MCB-SMART-BOY/Neve/releases/download/v1.0.0/neve-x86_64-apple-darwin.tar.gz"
+      sha256 "659022822ec8611335e58090b03bd27b52506618cdeea4cdabf61d153963f8a7"
     end
 
     on_arm do
-      url "https://github.com/MCB-SMART-BOY/Neve/releases/download/v0.7.0/neve-aarch64-apple-darwin.tar.gz"
-      sha256 "fd2708779876414039651dfe94a944013bca48b44639117615b07f588a8bfade"
+      url "https://github.com/MCB-SMART-BOY/Neve/releases/download/v1.0.0/neve-aarch64-apple-darwin.tar.gz"
+      sha256 "5d8a304a97b98ab4b1ff52b410c28f817103427ed7929f671b8b9272dba4d44a"
     end
   end
 
