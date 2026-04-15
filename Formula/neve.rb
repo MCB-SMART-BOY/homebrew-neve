9792a9344d3ab9fb1d2e318d8f8e5452700b5c7e
MCB-SMART-BOY <2720838051@qq.com>
2026-04-15 21:06:23 +0800

    neve 3.0.0

--- Formula/neve.rb
+++ Formula/neve.rb
@@ -1,18 +1,18 @@
 class Neve < Formula
   desc "Standalone language for system configuration, builds, and shell automation"
   homepage "https://github.com/MCB-SMART-BOY/Neve"
-  version "2.0.0"
+  version "3.0.0"
   license "MPL-2.0"
 
   on_macos do
     on_intel do
-      url "https://github.com/MCB-SMART-BOY/Neve/releases/download/v2.0.0/neve-x86_64-apple-darwin.tar.gz"
-      sha256 "b3556d8fa561f24f873f677be99671467f021fea7fc74626af6fcc4aa20970fa"
+      url "https://github.com/MCB-SMART-BOY/Neve/releases/download/v3.0.0/neve-x86_64-apple-darwin.tar.gz"
+      sha256 "d2053860833cbe9394a03a50f81058519ccf4163ade24c49eb280f6b786bfcf3"
     end
 
     on_arm do
-      url "https://github.com/MCB-SMART-BOY/Neve/releases/download/v2.0.0/neve-aarch64-apple-darwin.tar.gz"
-      sha256 "f159b41e05600ea30830d653cdabd2bda84ccf78fe004add7f7916e1b8ad2659"
+      url "https://github.com/MCB-SMART-BOY/Neve/releases/download/v3.0.0/neve-aarch64-apple-darwin.tar.gz"
+      sha256 "01f198f04f5f9d5ac9e7006800e556a1096c41ad7e8895a817014c3103493b38"
     end
   end
 
